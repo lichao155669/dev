2f837628f8c389efdffa2c67acb103aec6d229d6
lichao155669 <1659639682@qq.com>
2025-08-12 16:55:11 +0800

    首次代码提交

--- code/pt10-cosmic-debug/src/main/java/pt10/cosmic/debug/DebugApplication.java
+++ code/pt10-cosmic-debug/src/main/java/pt10/cosmic/debug/DebugApplication.java
@@ -27,7 +27,8 @@ public class DebugApplication {
         
         cosmic.setConfigUrl("192.168.31.20:2181?user=zookeeper&password=d@f*g:SGVsbG8==8S/efRX0EtJa3d5P88i0tDsxRdGlh6nIJ7kL3nBJv1u1a2RwYXNzd29yZA==");
 //        cosmic.setConfigUrl("192.168.31.20:2181");
-        
+
+
 //        cosmic.setMcServerUrl("http://127.0.0.1:8090");
         cosmic.setMcServerUrl("http://192.168.31.20:8090"); 
         
